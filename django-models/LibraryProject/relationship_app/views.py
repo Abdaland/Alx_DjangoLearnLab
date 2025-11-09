8fc35f52441a84738382a1d68c85bfb777ba1b43
Abdaland <Abdallah-sonic@hotmail.fr>
2025-11-09 04:49:19 +0100

    Fix ALX Django project: role-based views and book permissions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test, permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Book, Library, Author
+from .models import Library, Book
+
 
 # --- Registration ---
 def register_view(request):
