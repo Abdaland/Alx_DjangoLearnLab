bf6ee4006a849a891179d00c1de952d3cf6a24c4
Abdaland <Abdallah-sonic@hotmail.fr>
2025-11-09 04:04:51 +0100

    Fix views, URLs, and templates for ALX Django project

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -33,3 +33,6 @@ urlpatterns = [
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
 ]
+path('books/add/', add_book, name='add_book'),
+path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+path('books/<int:pk>/delete/', delete_book, name='delete_book'),
