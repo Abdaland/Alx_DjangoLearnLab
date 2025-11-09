fbfc773d4d730fef2d7887bf9a8bd546d98d93bd
Abdaland <Abdallah-sonic@hotmail.fr>
2025-11-09 04:52:43 +0100

    Your commit message

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,29 +1,18 @@
 from django.urls import path
 from .views import (
-    register_view,
     list_books,
+    register_view,
     LibraryDetailView,
     admin_view,
     librarian_view,
-    member_view,
-    add_book,
-    edit_book
+    member_view
 )
 
 urlpatterns = [
-    # Authentication
     path('register/', register_view, name='register'),
-
-    # Books & Library
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-
-    # Role-based views
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
-
-    # Book permissions
-    path('add_book/', add_book, name='add_book'),
-    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
 ]
